7a4c98eaec51f73d2b25939b6d4b1bd5f101a99e
ivanp7 <ivanp7@mail.ru>
2021-12-05 00:26:44 +0300

    Improve sxhkd autostart script

--- xorg/sxhkd/XDG_CONFIG_HOME/bspwm/autostart/90-sxhkd.sh
+++ xorg/sxhkd/XDG_CONFIG_HOME/bspwm/autostart/90-sxhkd.sh
@@ -2,6 +2,6 @@
 
 while xhost > /dev/null 2>&1
 do
-    sxhkd $(find "$XDG_CONFIG_HOME/sxhkd/conf/" -type f,l | sort)
+    sxhkd $(find "$XDG_CONFIG_HOME/sxhkd/conf" -type f,l 2> /dev/null | sort)
 done
 
