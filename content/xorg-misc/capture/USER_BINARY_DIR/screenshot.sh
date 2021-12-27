66e353476158ec1bfbcbbca2cc04ab14891922cd
ivanp7 <ivanp7@mail.ru>
2021-12-27 15:49:29 +0300

    Improve screenshot capturing -- implement directory selection with dmenu

--- content/xorg-misc/capture/USER_BINARY_DIR/screenshot.sh
+++ content/xorg-misc/capture/USER_BINARY_DIR/screenshot.sh
@@ -3,17 +3,39 @@
 if [ ! -t 1 ] # pipe
 then
     maim "$@"
-    exit
+    exit "$?"
 fi
+
+NOTIFY_TIME=3000
 
 : ${FILENAME_PREFIX:="screenshot"}
 : ${FILENAME:="${FILENAME_PREFIX}_$(date "+%F_%T").png"}
 
-: ${CAPTURE_DIRECTORY:="screenshot"}
-: ${DIRECTORY:="$HOME/capture/$CAPTURE_DIRECTORY"}
+DIRECTORY="$TMPDIR_SESSION/screenshot"
+
 mkdir -p -- "$DIRECTORY"
 
-maim "$@" "$DIRECTORY/$FILENAME" &&
-    notify-send -t 3000 "Screenshot saved as $DIRECTORY/$FILENAME" ||
-    notify-send -u critical -t 2000 "Error, screenshot not saved"
+if ! maim "$@" -- "$DIRECTORY/$FILENAME"
+then
+    notify-send -u critical -t $NOTIFY_TIME "Error, screenshot not saved"
+    exit 1
+fi
+
+DMENU_PROMPT="Screenshot directory"
+DMENU_LINES=10
+DMENU_COLUMNS=4
+
+HISTORY_FILE="$XDG_CACHE_HOME/screenshot-history"
+touch -- "$HISTORY_FILE"
+
+INPUT=$(tac -- "$HISTORY_FILE" | dmenu.sh -p "$DMENU_PROMPT" -l $DMENU_LINES -g $DMENU_COLUMNS | head -1)
+if [ -z "$INPUT" ] || ! cp -t "$INPUT" -- "$DIRECTORY/$FILENAME"
+then
+    notify-send -t $NOTIFY_TIME "Screenshot saved as $DIRECTORY/$FILENAME"
+    exit
+fi
+
+{ grep -Fv "$INPUT" -- "$HISTORY_FILE"; echo "$INPUT"; } | sponge -- "$HISTORY_FILE"
+
+notify-send -t $NOTIFY_TIME "Screenshot saved as $INPUT/$FILENAME"
 
