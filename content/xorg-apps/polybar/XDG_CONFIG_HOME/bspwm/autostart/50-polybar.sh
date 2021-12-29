1fecdce7742a80c67073ec93d3c07f198fcebb36
ivanp7 <ivanp7@mail.ru>
2021-12-29 17:39:40 +0300

    Improve polybar configuration

--- content/xorg-apps/polybar/XDG_CONFIG_HOME/bspwm/autostart/50-polybar.sh
+++ content/xorg-apps/polybar/XDG_CONFIG_HOME/bspwm/autostart/50-polybar.sh
@@ -3,26 +3,20 @@
 export WINDOW_TITLE_FORMAT="%title:0:$(($(bspwm-monitor-info.sh area | cut -d' ' -f1) / $DEFAULT_FONT_WIDTH * 5/12)):...%"
 export FONT="$DEFAULT_FONT;0"
 
-export SCRIPT_TTY="$XDG_CONFIG_HOME/polybar/tty.sh"
+export PREFIX="$VT:$(id -un)"
 
 keep_polybar ()
 {
     while xhost > /dev/null 2>&1
     do
-        if xrandr | grep -q "^$1 .* [0-9]\+x[0-9]\++[0-9]\++[0-9]\+ "
-        then
-            [ "$1" = "$PRIMARY_MONITOR" ] &&
-                MONITOR="$1" TRAY_POS=right polybar --reload top ||
-                MONITOR="$1" polybar --reload top
-        else
-            sleep 8
-        fi
+        xrandr | grep -q "^$1 .* [0-9]\+x[0-9]\++[0-9]\++[0-9]\+ " &&
+            MONITOR="$1" TRAY_POS="$2" polybar --reload top || sleep 8
         sleep 2
     done
 }
 
 for m in $(polybar --list-monitors | cut -d":" -f1)
 do
-    keep_polybar "$m" &
+    keep_polybar "$m" "$([ "$m" = "$PRIMARY_MONITOR" ] && echo right || echo none)" &
 done
 
