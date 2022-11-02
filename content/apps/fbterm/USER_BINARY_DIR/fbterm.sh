cd852e6f660908b393461a4e59e0a13a5a36744b
ivanp7 <ivanp7@mail.ru>
2022-11-02 10:38:44 +0300

    chore: add dot-files for framebuffer applications

---
+++ content/apps/fbterm/USER_BINARY_DIR/fbterm.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+[ "$TERM" = "linux" ] || exit
+
+# request display of shell info
+PIDFILE="$TMPDIR_SESSION/shell_info/$(tty)/$PARENT_SHELL_PID_EXPORTED"
+[ -n "$PARENT_SHELL_PID_EXPORTED" -a -f "$PIDFILE" ] && rm "$PIDFILE" || true
+
+: ${FBTERM_WALLPAPER:="$HOME/wallpapers/fbterm"}
+
+if [ -e "$FBTERM_WALLPAPER" ] && file --mime-type "$FBTERM_WALLPAPER" -bLE | grep -q "^image/"
+then
+    echo -ne "\e[?25l" # hide cursor
+    fbv -ciuker "$FBTERM_WALLPAPER" << EOF
+q
+EOF
+    export FBTERM_BACKGROUND_IMAGE=1
+fi
+
+exec fbterm -- "$XDG_CONFIG_HOME/fbterm/init.sh" "$1"
+
