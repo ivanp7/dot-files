4805d950811ca888a40a3e5a65f4aab7d82682d4
ivanp7 <ivanp7@mail.ru>
2022-11-04 13:36:42 +0300

    feat: improve bspwm desktops control

--- content/apps/bspwm/USER_BINARY_DIR/bspwm-desktops.sh
+++ content/apps/bspwm/USER_BINARY_DIR/bspwm-desktops.sh
@@ -1,40 +1,50 @@
 #!/bin/sh
 
+desktop_name ()
+{
+    case $1 in
+        1) echo "${DESKTOP_1:-"I"}" ;;
+        2) echo "${DESKTOP_2:-"II"}" ;;
+        3) echo "${DESKTOP_3:-"III"}" ;;
+        4) echo "${DESKTOP_4:-"IV"}" ;;
+        5) echo "${DESKTOP_5:-"V"}" ;;
+        6) echo "${DESKTOP_6:-"VI"}" ;;
+        7) echo "${DESKTOP_7:-"VII"}" ;;
+        8) echo "${DESKTOP_8:-"VIII"}" ;;
+        9) echo "${DESKTOP_9:-"IX"}" ;;
+        10) echo "${DESKTOP_10:-"X"}" ;;
+    esac
+}
+
 MONITOR=$2
 
 N=$(bspc query -D -m $MONITOR | wc -l)
 
-: ${DESKTOP_1:="I"}
-: ${DESKTOP_2:="II"}
-: ${DESKTOP_3:="III"}
-: ${DESKTOP_4:="IV"}
-: ${DESKTOP_5:="V"}
-: ${DESKTOP_6:="VI"}
-: ${DESKTOP_7:="VII"}
-: ${DESKTOP_8:="VIII"}
-: ${DESKTOP_9:="IX"}
-: ${DESKTOP_10:="X"}
-
-desktops ()
+add_desktop ()
 {
-    case $1 in
-        0|1) echo "$DESKTOP_1" ;;
-        2) echo "$DESKTOP_1 $DESKTOP_2" ;;
-        3) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3" ;;
-        4) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3 $DESKTOP_4" ;;
-        5) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3 $DESKTOP_4 $DESKTOP_5" ;;
-        6) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3 $DESKTOP_4 $DESKTOP_5 $DESKTOP_6" ;;
-        7) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3 $DESKTOP_4 $DESKTOP_5 $DESKTOP_6 $DESKTOP_7" ;;
-        8) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3 $DESKTOP_4 $DESKTOP_5 $DESKTOP_6 $DESKTOP_7 $DESKTOP_8" ;;
-        9) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3 $DESKTOP_4 $DESKTOP_5 $DESKTOP_6 $DESKTOP_7 $DESKTOP_8 $DESKTOP_9" ;;
-        10|*) echo "$DESKTOP_1 $DESKTOP_2 $DESKTOP_3 $DESKTOP_4 $DESKTOP_5 $DESKTOP_6 $DESKTOP_7 $DESKTOP_8 $DESKTOP_9 $DESKTOP_10" ;;
-    esac
+    [ "$N" -lt 10 ] || return
+    bspc monitor $MONITOR -a "$(desktop_name $(($N + 1)))" && N=$((N + 1))
+}
+
+del_desktop ()
+{
+    [ "$N" -gt 1 ] || return
+    local desktop_id=$(bspc query -m $MONITOR -D | tail -1)
+    bspc desktop $desktop_id -r && N=$(($N - 1))
 }
 
 case $1 in
     "") echo $N ;;
-    inc) [ "$N" -lt 10 ] && bspc monitor $MONITOR -d $(desktops $(($N + 1))) ;;
-    dec) [ "$N" -gt 1 ] && bspc monitor $MONITOR -d $(desktops $(($N - 1))) ;;
-    *) bspc monitor $MONITOR -d $(desktops $1) ;;
+    inc) add_desktop ;;
+    dec) del_desktop ;;
+    *)
+        delta=$(($1 - $N))
+        if [ "$delta" -gt 0 ]
+        then
+            for i in $(seq $delta); do add_desktop || exit 1; done
+        elif [ "$delta" -lt 0 ]
+        then
+            for i in $(seq $delta -1); do del_desktop || exit 1; done
+        fi
 esac
 
