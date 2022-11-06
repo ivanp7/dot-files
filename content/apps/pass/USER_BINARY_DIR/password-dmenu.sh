622e10fc5379456e7d1b79b840cc28a75ee8a1a8
ivanp7 <ivanp7@mail.ru>
2022-11-06 18:38:40 +0300

    feat: improve password-dmenu.sh

--- content/apps/pass/USER_BINARY_DIR/password-dmenu.sh
+++ content/apps/pass/USER_BINARY_DIR/password-dmenu.sh
@@ -7,7 +7,9 @@ DMENU_COLUMNS=3
 cd -- "$PASSWORD_STORE_DIR"
 
 export PINENTRY_USER_DATA=X
-find . -path "./.git" -prune -o -type f -name "*.gpg" -print |
-    sed 's@^\./@@; s@\.gpg@@' |
-    dmenu.sh -p "$DMENU_PROMPT" -l $DMENU_LINES -g $DMENU_COLUMNS | head -1 | xargs -r pass -c
+
+NAME="$(find . -path "./.git" -prune -o -type f -name "*.gpg" -print | sed 's@^\./@@; s@\.gpg@@' |
+    dmenu.sh -p "$DMENU_PROMPT" -l $DMENU_LINES -g $DMENU_COLUMNS | head -1)"
+
+[ "$NAME" ] && pass -c "$NAME"
 
