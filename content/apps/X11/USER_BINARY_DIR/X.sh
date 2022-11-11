a07f20170510b05ffa4e264912b0b3174b4f2e18
ivanp7 <ivanp7@mail.ru>
2022-11-11 15:08:52 +0300

    refactor: improve X.sh

--- content/apps/X11/USER_BINARY_DIR/X.sh
+++ content/apps/X11/USER_BINARY_DIR/X.sh
@@ -3,8 +3,7 @@
 cd
 
 # request display of shell info
-PIDFILE="$TMPDIR_SESSION/shell_info/$(tty)/$PARENT_SHELL_PID_EXPORTED"
-[ -n "$PARENT_SHELL_PID_EXPORTED" -a -f "$PIDFILE" ] && rm "$PIDFILE" || true
+[ "$ZDOTDIR" ] && "$ZDOTDIR/aux/request_shell_info.sh"
 
 # server start
 exec startx "$XDG_CONFIG_HOME/X11/xinitrc" -- "$XDG_CONFIG_HOME/X11/xserverrc"
