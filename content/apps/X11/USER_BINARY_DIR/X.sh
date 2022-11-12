a320d9cd3385a7e447cc1e3559417ef23a637ee0
ivanp7 <ivanp7@mail.ru>
2022-11-12 10:18:22 +0300

    refactor: improve shell portability of X.sh

--- content/apps/X11/USER_BINARY_DIR/X.sh
+++ content/apps/X11/USER_BINARY_DIR/X.sh
@@ -2,9 +2,6 @@
 
 cd
 
-# request display of shell info
-[ "$ZDOTDIR" ] && "$ZDOTDIR/aux/request_shell_info.sh"
-
 # server start
 exec startx "$XDG_CONFIG_HOME/X11/xinitrc" -- "$XDG_CONFIG_HOME/X11/xserverrc"
 
