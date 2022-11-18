0f743bf97480c0416f0db27bcbb9475830f5dd9f
ivanp7 <ivanp7@mail.ru>
2022-11-18 13:45:41 +0300

    fix(surf): do not open empty surf tab when arguments supplied

--- content/apps/surf/USER_BINARY_DIR/tabbed-surf.sh
+++ content/apps/surf/USER_BINARY_DIR/tabbed-surf.sh
@@ -3,12 +3,13 @@
 : ${TABBED_CLASS:="surf_$(($(xdotool get_desktop)+1))"}
 export TABBED_CLASS
 
-TABBED_COMMAND_XID_ARG=2 tabbed.sh surf -e xid # default command for tabbed
 if [ "$#" -gt 0 ]
 then
     for url in "$@"
     do
         surf.sh -e "$(TABBED_COMMAND_XID_ARG= tabbed.sh)" -- "$url"
     done
+else
+    TABBED_COMMAND_XID_ARG=2 tabbed.sh surf -e xid # default command for tabbed
 fi
 
